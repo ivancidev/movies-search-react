f827957bead07405f23ace9ec7257f3656d16294
Herlan <herlanherbas@gmail.com>
2024-07-08 23:16:58 -0400

    create page Movies

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from "react";
+import SearchAppBar from "../components/Search";
+import ActionAreaCard from "../components/Card";
+import "./movies.css";
+import { API_KEY, URL } from "../config/env";
+
 
 export default function Movies() {
+  const [movies, setMovies] = React.useState([]);
+
+  const handleChange = async (search) => {
+    const response = await fetch(`${URL}?query=${search}&api_key=${API_KEY}`);
+    const data = await response.json();
+    setMovies(data.results);
+  };
+
   return (
-    <div>Movies</div>
-  )
+    <>
+      <SearchAppBar onSearch={handleChange} />
+      <div className="movies">
+        {movies.map((movie) => (
+          <ActionAreaCard
+            key={movie.id}
+            title={movie.title}
+            poster_path={movie.poster_path}
+            vote_average={movie.vote_average}
+            overview={movie.overview}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
